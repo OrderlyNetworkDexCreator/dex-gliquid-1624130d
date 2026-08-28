fb00af88388ba839c8f88f5570fa3d86bc36d45c
orderly-guoqing <guoqing@orderly.network>
2026-08-28 10:44:19 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_WALLETCONNECT_PROJECT_ID": "",
   "VITE_APP_NAME": "gliquid",
   "VITE_APP_DESCRIPTION": "the chain to house all goon-related finance. trade on the goon-performant chain.",
-  "VITE_HAS_PRIMARY_LOGO": "false",
+  "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
   "VITE_CUSTOM_MENUS": "",
